15d1a338336b8054b71412de083316db9477ebd9
Kourosh394 <kouroshfathabadi94@gmail.com>
2026-09-17 15:42:51 +0330

    Add files via upload

--- faprgs/کلمه 5 حرف حدس زن/shared js.js
+++ faprgs/کلمه 5 حرف حدس زن/shared js.js
@@ -4,3 +4,21 @@ function MAW() {
     document.getElementById("money").innerText = `💵: ${money}`;
     document.getElementById("words").innerText = `کلمات: ${words}`;
 }
+function shake(id) {
+    setTimeout(function () {
+        document.getElementById(id).classList.add("shake");
+    }, 500)
+    document.getElementById(id).classList.remove("shake");
+}
+function SFTTXS(id) {
+    setTimeout(function () {
+        document.getElementById(id).classList.add("SFTTXS");
+    }, 500)
+    document.getElementById(id).classList.remove("SFTTXS");
+}
+function SH1(id) {
+    setTimeout(function () {
+        document.getElementById(id).classList.add("SH1");
+    }, 500)
+    document.getElementById(id).classList.remove("SH1");
+}
