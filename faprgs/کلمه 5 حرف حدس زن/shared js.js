a38886e59de84dede7813431413d3c329bcf2dce
Kourosh394 <kouroshfathabadi94@gmail.com>
2026-09-20 18:11:17 +0330

    Add files via upload

--- faprgs/کلمه 5 حرف حدس زن/shared js.js
+++ faprgs/کلمه 5 حرف حدس زن/shared js.js
@@ -1,5 +1,9 @@
 let money = Number(localStorage.getItem("money-g"));
 let words = Number(localStorage.getItem("words-g"));
+if (window.location.hostname != "kourosh394.github.io" && window.location.protocol != "file:") {
+    alert(`این بازی تقلبی کلمه 5 حرف حدس زن است. لینک تقلبی: ${decodeURIComponent(window.location.href)} لینک اصلی: https://kourosh394.github.io/HTMLCFF/faprgs/کلمه 5 حرف حدس زن/main.html OK را بزنید تا به نسخه ی اصلی بروید.`);
+    window.location.href = "https://kourosh394.github.io/HTMLCFF/faprgs/کلمه 5 حرف حدس زن/main.html";
+}
 function MAW() {
     document.getElementById("money").innerText = `💵: ${money}`;
     localStorage.setItem("money-g", money);
